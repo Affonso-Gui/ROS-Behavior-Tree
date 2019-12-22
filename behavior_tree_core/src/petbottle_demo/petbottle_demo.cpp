265cb383345e20ead4993be69c2dae4cf5e5b9e9
Guilherme Affonso <guilherme.c.affonso@gmail.com>
2019-12-22 12:27:00 +0900

    Use implicit sequence

--- behavior_tree_core/src/petbottle_demo/petbottle_demo.cpp
+++ behavior_tree_core/src/petbottle_demo/petbottle_demo.cpp
@@ -5,43 +5,44 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "petbottle_demo");
     try
     {
-        int TickPeriod_milliseconds = 1000;
+        int TickPeriod_milliseconds = 100;
 
 
         BT::ROSAction* pick_action = new BT::ROSAction("pick");
         BT::ROSAction* search_action = new BT::ROSAction("search");
         BT::ROSAction* finish_action = new BT::ROSAction("finish");
 
-        BT::ROSCondition* finishing_cond = new BT::ROSCondition("finishing");
-        BT::ROSCondition* lost_cond = new BT::ROSCondition("lost");
+        BT::ROSCondition* found_cond = new BT::ROSCondition("found");
         BT::ROSCondition* grasped_cond = new BT::ROSCondition("grasped");
         BT::ROSCondition* moved_cond = new BT::ROSCondition("moved");
 
-        BT::NegationNode* decorator_lost = new BT::NegationNode("decorator1");
         BT::NegationNode* decorator_moved = new BT::NegationNode("decorator2");
 
-        BT::SequenceNode* sequence_root = new BT::SequenceNode("seq_root");
+        BT::SequenceNode* sequence_finish = new BT::SequenceNode("seq_finish");
         BT::SequenceNode* sequence_pick = new BT::SequenceNode("seq_pick");
-        BT::FallbackNode* fallback_grasp = new BT::FallbackNode("fall_grasp");
-        BT::FallbackNode* fallback_search = new BT::FallbackNode("fall_search");
+        BT::SequenceNode* sequence_pick_moved = new BT::SequenceNode("seq_pick_moved");
 
-        sequence_root->AddChild(fallback_search);
-        sequence_root->AddChild(fallback_grasp);
-        sequence_root->AddChild(finish_action);
+        BT::FallbackNode* fallback_grasped = new BT::FallbackNode("fall_grasped");
+        BT::FallbackNode* fallback_found = new BT::FallbackNode("fall_found");
 
-        fallback_search->AddChild(finishing_cond);
-        fallback_search->AddChild(decorator_lost);
-          decorator_lost->AddChild(lost_cond);
-        fallback_search->AddChild(search_action);
+        sequence_finish->AddChild(fallback_grasped);
+        sequence_finish->AddChild(finish_action);
 
-        fallback_grasp->AddChild(grasped_cond);
-        fallback_grasp->AddChild(sequence_pick);
-          sequence_pick->AddChild(decorator_moved);
-            decorator_moved->AddChild(moved_cond);
-          sequence_pick->AddChild(pick_action);
+        fallback_grasped->AddChild(grasped_cond);
+        fallback_grasped->AddChild(sequence_pick);
 
-        sequence_root->set_reset_policy(BT::ON_FAILURE);
-        Execute(sequence_root, TickPeriod_milliseconds);  // from BehaviorTree.cpp
+        sequence_pick->AddChild(fallback_found);
+        sequence_pick->AddChild(sequence_pick_moved);
+
+        fallback_found->AddChild(found_cond);
+        fallback_found->AddChild(search_action);
+
+        sequence_pick_moved->AddChild(decorator_moved);
+          decorator_moved->AddChild(moved_cond);
+        sequence_pick_moved->AddChild(pick_action);
+
+        sequence_finish->set_reset_policy(BT::ON_FAILURE);
+        Execute(sequence_finish, TickPeriod_milliseconds);  // from BehaviorTree.cpp
     }
     catch (BT::BehaviorTreeException& Exception)
     {
